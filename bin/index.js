a1d8ec9c2b080e751bc83f755dbe073c0f6065b4
Illia Olenchenko <icrosil@gmail.com>
2019-06-08 11:11:44 +0300

    added eslint

--- bin/index.js
+++ bin/index.js
@@ -5,18 +5,18 @@ const gitP = require('simple-git/promise');
 const readdirp = require('readdirp');
 const FuzzySearch = require('fuzzy-search');
 
-const package = require('../package.json');
+const pack = require('../package.json');
 
 const allDirsPassed = [];
 
 program
-  .version(package.version)
+  .version(pack.version)
   .option('-w, --workdir <String>', 'git repo', './')
   .arguments('<dir> [otherDirs...]')
-  .action(function (dirPassed, otherDirsPassed = []) {
-    const addDirs = (currentDir) => {
+  .action((dirPassed, otherDirsPassed = []) => {
+    const addDirs = currentDir => {
       allDirsPassed.push(currentDir.split(';'));
-    }
+    };
     [dirPassed, ...otherDirsPassed].forEach(addDirs);
   });
 
@@ -31,48 +31,48 @@ if (!allDirsPassed.length) {
 
 // TODO catch error
 // TODO modulize code
-git.checkIsRepo()
-  .then(async (isRepo) => {
-    // TODO check everything are fine to proceed, create branches
-    // TODO stop proceed if not repo
-    // TODO maybe i need to define root of repo 'git rev-parse --show-toplevel'
-    // console.log(`dir ${program.workdir} is repo = ${isRepo}`);
-    // const repoStatus = await git.status();
-    // TODO stop proceed if not clean
-    // console.log({ repoStatus });
-    const dirs = await readdirp.promise(program.workdir, {
-      type: 'directories',
-      depth: 5, // TODO configurable and proceed deeper if not found
-      directoryFilter: ['!.git', '!*modules'] // TODO also configurable?
-    });
-    // TODO can i avoid reading all dirs before searching and check on search?
-    // TODO also good optimization is to cache already read dirs and continue on new request
-    // TODO do for all dirs
-    // TODO support multiple paths in 1 dir by ;
-    const fuzzy = new FuzzySearch(dirs, ['path']);
-    const bestDirsPaths = allDirsPassed.map((currentDirs) => {
-      return currentDirs.map(currentDir => {
-        const [bestDirFuse] = fuzzy.search(currentDir); // TODO if bestDir exist (what if not)
-        console.log(`best match for ${currentDir} is ${bestDirFuse.path}`);
-        return bestDirFuse.path;
-      });
-    });
-    console.log({ bestDirsPaths });
-    // TODO log current branch
-    // TODO compare 2 branches by each dir like `git rev-list --reverse --no-merges develop..HEAD -- projects/login`
-    // TODO add options to choose branch from to
-    // TODO should I include --no-merges or no?
-    // TODO check everything passed exist
-    // const revListRaw = await git.raw(['rev-list', '--reverse', '--no-merges', 'develop..HEAD', '--', 'projects/login']);
-    // const revListExample = (revListRaw || []).split('\n').slice(0, -1);
-    // console.log({ revListExample });
-    // TODO create branch per dir (check if not exists)
-    // TODO run gco HASH DIRS (this will add files from dirs to unstaged)
-    // TODO stage
-    // TODO get commit message by HASH - git show-branch --no-name HASH
-    // TODO commit given files with given message
-    // TODO repeat per HASH
-    // TODO repeat per branch
+git.checkIsRepo().then(async isRepo => {
+  // TODO check everything are fine to proceed, create branches
+  // TODO stop proceed if not repo
+  // TODO maybe i need to define root of repo 'git rev-parse --show-toplevel'
+  console.log(`dir ${program.workdir} is repo = ${isRepo}`);
+  // const repoStatus = await git.status();
+  // TODO stop proceed if not clean
+  // console.log({ repoStatus });
+  const dirs = await readdirp.promise(program.workdir, {
+    type: 'directories',
+    depth: 5, // TODO configurable and proceed deeper if not found
+    directoryFilter: ['!.git', '!*modules'], // TODO also configurable?
   });
+  // TODO can i avoid reading all dirs before searching and check on search?
+  // TODO also good optimization is to cache already read dirs and continue on new request
+  // TODO do for all dirs
+  // TODO support multiple paths in 1 dir by ;
+  const fuzzy = new FuzzySearch(dirs, ['path']);
+  const bestDirsPaths = allDirsPassed.map(currentDirs => {
+    return currentDirs.map(currentDir => {
+      const [bestDirFuse] = fuzzy.search(currentDir); // TODO if bestDir exist (what if not)
+      console.log(`best match for ${currentDir} is ${bestDirFuse.path}`);
+      return bestDirFuse.path;
+    });
+  });
+  console.log({ bestDirsPaths });
+  // TODO log current branch
+  // TODO compare 2 branches by each dir like `git rev-list --reverse --no-merges develop..HEAD -- projects/login`
+  // TODO add options to choose branch from to
+  // TODO should I include --no-merges or no?
+  // TODO check everything passed exist
+  // const revListRaw = await git.raw(['rev-list', '--reverse', '--no-merges',
+  //  'develop..HEAD', '--', 'projects/login']);
+  // const revListExample = (revListRaw || []).split('\n').slice(0, -1);
+  // console.log({ revListExample });
+  // TODO create branch per dir (check if not exists)
+  // TODO run gco HASH DIRS (this will add files from dirs to unstaged)
+  // TODO stage
+  // TODO get commit message by HASH - git show-branch --no-name HASH
+  // TODO commit given files with given message
+  // TODO repeat per HASH
+  // TODO repeat per branch
+});
 
 console.log('Dirs passed:', ...allDirsPassed);
