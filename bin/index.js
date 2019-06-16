3bdcc6f3469f0bfc42297f3ea627c511239b889d
Illia Olenchenko <icrosil@gmail.com>
2019-06-16 16:40:30 +0300

    added separate by semicolon as default for branches

--- bin/index.js
+++ bin/index.js
@@ -7,15 +7,18 @@ const fuzzDirs = require('../src/fuzzDirs');
 const repo = require('../src/repo');
 const { error, initLog, notice } = require('../src/log');
 
+function semicolonSeparatedList(value) {
+  return value.split(';');
+}
+
 // TODO add more logs (comparing what branch to what) creating branch from etc
-// TODO to dirs and commits separated by ';' we could give handlers on read to avoid it later
 program
   .version(pack.version)
   .option('-w, --workdir <directory>', 'git repo', './')
   .option('-R, --no-root', 'should we use git root or directory')
   .option('-f, --from <type>', 'branch you would like to compare from', 'develop')
   .option('-t, --to <branch>', 'branch you would like to compare to')
-  .option('-b, --branches <branches>', 'branches you would like to commit to, separated by ";"')
+  .option('-b, --branches <branches>', 'branches you would like to commit to, separated by ";"', semicolonSeparatedList)
   .option('-d, --debug [level]', 'logging extra information just in case, level could be specified')
   .arguments('<dir> [otherDirs...]')
   .action(async (dir, otherDirs, options) => {
