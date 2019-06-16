fdb5a9ea7bacbae7f3cec676386da0b5d27f5673
Illia Olenchenko <icrosil@gmail.com>
2019-06-16 17:41:43 +0300

    Added prompt if depth not enough

--- src/fuzzDirs.js
+++ src/fuzzDirs.js
@@ -1,7 +1,9 @@
 const readdirp = require('readdirp');
 const FuzzySearch = require('fuzzy-search');
+const inquirer = require('inquirer');
 
 const { notice, info } = require('./log');
+const sequential = require('./utils/sequential');
 
 const fuzzDirs = async (workdir, dirsToLookup, options) => {
   if (!workdir) {
@@ -18,19 +20,35 @@ const fuzzDirs = async (workdir, dirsToLookup, options) => {
     directoryFilter: ['!.git', '!*modules'],
   });
   const fuzzy = new FuzzySearch(dirs, ['path']);
-  const bestDirsPaths = dirsToLookup.map(currentDirs => {
-    return currentDirs.map(currentDir => {
-      // TODO if bestDir exist (what if not)
-      // TODO add ability to select with arrows if best choice not found correctly
-      const [bestDirFuse] = fuzzy.search(currentDir);
-      if (!bestDirFuse || !bestDirFuse.path) {
-        // TODO what if it is nested deeper than 5
-        throw new Error(`workdir ${workdir} does not contain directory like ${currentDir}`);
-      }
-      info(`best match for ${currentDir} is ${bestDirFuse.path}`);
-      return bestDirFuse.path;
-    });
-  });
+  const bestDirsPaths = await sequential(
+    dirsToLookup.map(currentDirs => async () => {
+      return sequential(
+        currentDirs.map(currentDir => async () => {
+          // TODO if bestDir exist (what if not)
+          // TODO add ability to select with arrows if best choice not found correctly
+          const [bestDirFuse] = fuzzy.search(currentDir);
+          if (!bestDirFuse || !bestDirFuse.path) {
+            if (!options.maximumDepth) {
+              const answer = await inquirer.prompt([
+                {
+                  message: `We didn't found any matching dir, do you want to go deeper than ${options.findDepth}?`,
+                  type: 'confirm',
+                  name: 'goDeep',
+                  default: true,
+                },
+              ]);
+              if (answer.goDeep) {
+                throw new Error('go deeper');
+              }
+            }
+            throw new Error(`workdir ${workdir} does not contain directory like ${currentDir}`);
+          }
+          info(`best match for ${currentDir} is ${bestDirFuse.path}`);
+          return bestDirFuse.path;
+        }),
+      );
+    }),
+  );
   notice(bestDirsPaths.join(' & '));
   return bestDirsPaths;
 };
@@ -39,6 +57,19 @@ module.exports = async (workdir, dirsToLookup = [], options) => {
   const allDirsPassed = dirsToLookup.map(currentDir => {
     return currentDir.split(';');
   });
-  const foundDirs = await fuzzDirs(workdir, allDirsPassed, options);
-  return foundDirs;
+  try {
+    const foundDirs = await fuzzDirs(workdir, allDirsPassed, options);
+    return foundDirs;
+  } catch (error) {
+    if (error.message === 'go deeper') {
+      notice(`Going deeper, next level - ${options.findDepth + 3}`);
+      const foundDirs = await fuzzDirs(
+        workdir,
+        allDirsPassed,
+        Object.assign({}, options, { findDepth: options.findDepth + 3, maximumDepth: true }),
+      );
+      return foundDirs;
+    }
+  }
+  return null;
 };
